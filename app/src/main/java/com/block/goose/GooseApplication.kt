c7ddde9d69012774936313dc1a1eb86e28a74108
madgeret-del <madgeret@gmail.com>
2026-07-11 17:02:35 +0300

    Integrate AgnesAiService into GooseApplication
    Added AgnesAiService initialization in GooseApplication.

--- app/src/main/java/com/block/goose/GooseApplication.kt
+++ app/src/main/java/com/block/goose/GooseApplication.kt
@@ -1,28 +1,35 @@
 package com.block.goose
 
 import android.app.Application
+import com.block.goose.data.api.AgnesAiService
 import com.block.goose.data.api.GooseApiService
 import com.block.goose.data.api.SettingsRepository
 
 class GooseApplication : Application() {
-    
     lateinit var settingsRepository: SettingsRepository
-        private set
-    
+    private set
     lateinit var apiService: GooseApiService
-        private set
-    
+    private set
+    lateinit var agnesAiService: AgnesAiService  // ⬅️ НОВОЕ
+    private set
+
     override fun onCreate() {
         super.onCreate()
         instance = this
-        
+
         // Initialize repositories and services
         settingsRepository = SettingsRepository(this)
         apiService = GooseApiService(settingsRepository)
+        
+        // ⬅️ НОВОЕ: Инициализация Agnes AI
+        agnesAiService = AgnesAiService(
+            baseUrl = settingsRepository.baseUrl,
+            apiKey = settingsRepository.secretKey
+        )
     }
-    
+
     companion object {
         lateinit var instance: GooseApplication
-            private set
+        private set
     }
 }
